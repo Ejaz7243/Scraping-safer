00f0060d26c6073752df6e3ec5885d4d4c0c612a
Ejaz Hassan <ejaz7243@gmail.com>
2024-10-09 21:20:31 +0500

    uncomment some changing in code

--- safer.py
+++ safer.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import pandas as pd
-# import time
+import time
 
 
 chrome_options = Options()
